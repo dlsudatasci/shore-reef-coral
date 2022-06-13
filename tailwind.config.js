b3b933f53ccaa218636eeabd7af632a0b96f7f77
Jared Sy <jaredblase@gmail.com>
2022-06-13 17:34:28 +0800

    add: google font

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,18 +1,24 @@
+const defaultTheme = require('tailwindcss/defaultTheme')
+
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-	content: [
+  content: [
     "./pages/**/*.{js,ts,jsx,tsx}",
     "./components/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-		colors: {
-			'primary': '#17829F',
-			'secondary': '#E7E1DA',
-			'accent-1': '#3CA6A9',
-			'accent-2': '#2D7E84',
-			'highlight': '#E3AD58',
-		},
-    extend: {},
+    extend: {
+      colors: {
+        primary: "#17829F",
+        secondary: "#E7E1DA",
+        "accent-1": "#3CA6A9",
+        "accent-2": "#2D7E84",
+        highlight: "#E3AD58",
+      },
+			fontFamily: {
+				'sans': ['Josefin Sans', ...defaultTheme.fontFamily.sans]
+			}
+    },
   },
   plugins: [],
-}
+};
