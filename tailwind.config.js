d76e8f24affdd0440d0c09e5cc2e5534677f0e0f
Jared Sy <jaredblase@gmail.com>
2022-06-13 16:51:47 +0800

    setup: TailwindCSS

---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+	content: [
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+		colors: {
+			'primary': '#17829F',
+			'secondary': '#E7E1DA',
+			'accent-1': '#3CA6A9',
+			'accent-2': '#2D7E84',
+			'highlight': '#E3AD58',
+		},
+    extend: {},
+  },
+  plugins: [],
+}
