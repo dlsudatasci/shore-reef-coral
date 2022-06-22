2834054b631bc4ee8406c828128125bee779d8ab
Jared Sy <jaredblase@gmail.com>
2022-06-22 15:29:33 +0800

    finish: dashboard

--- components/dashboard-bg.tsx
+++ components/dashboard-bg.tsx
@@ -2,8 +2,8 @@ import { FC } from 'react';
 
 const DashboardLayout: FC<{ children: React.ReactNode }> = ({ children }) => {
 	return (
-		<div className="bg-cover bg-no-repeat bg-primary" style={{ backgroundImage: 'url(beach-bg.jpg)'}}>
-			{children}
+		<div className="bg-cover bg-no-repeat bg-primary" style={{ backgroundImage: 'url(beach-bg.jpg)' }}>
+			<div className="container mx-auto px-4 xl:px-16">{children}</div>
 		</div>
 	)
 }
--- pages/dashboard/index.tsx
+++ pages/dashboard/index.tsx
@@ -2,6 +2,7 @@ import { NextPage } from 'next';
 import { signIn, useSession } from 'next-auth/react'
 import Image from 'next/image';
 import { useRouter } from 'next/router';
+import Card from '../../components/card';
 import DashboardLayout from '../../components/dashboard-bg';
 
 const Dashboard: NextPage = () => {
@@ -14,9 +15,15 @@ const Dashboard: NextPage = () => {
 
 	return (
 		<DashboardLayout>
-			<div className="flex">
-				<Image className="mr-4" src="/mask-light.png" alt="Mask Icon" width={50} height={50} />
-				<h1 className="font-comic-cat text-secondary">Dashboard</h1>
+			<div className="flex items-center mt-20">
+				<Image src="/mask-light.png" alt="Mask Icon" width={60} height={60} />
+				<h1 className="ml-4 font-comic-cat text-secondary">Dashboard</h1>
+			</div>
+			<h2 className="text-secondary font-comic-cat mt-8">Team Name</h2>
+			<div className="grid md:grid-cols-3 gap-4 mt-12">
+				<Card number={5} noun="Images" verb="Submitted" />
+				<Card number={40} noun="Butterfly Fish" verb="Found" />
+				<Card number={80} noun="Target Invertebrates" verb="Found	" />
 			</div>
 		</DashboardLayout>
 	)
