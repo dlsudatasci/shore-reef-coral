a2a2027056a066b1720cb10f16774364621b6cb9
Jared Sy <jaredblase@gmail.com>
2022-06-26 18:13:58 +0800

    add: profile layout

---
+++ components/layouts/profile-layout.tsx
@@ -0,0 +1,31 @@
+import Link from 'next/link'
+import { FC, ReactNode } from 'react'
+import DashboardLayout from './dashboard-layout'
+
+const navItems = [
+	{ name: 'Profile', path: '/dashboard/profile' },
+	{ name: 'Change password', path: '/dashboard/password' },
+	{ name: 'Team information', path: '/dashboard/team' },
+] as const
+
+const ProfileLayout: FC<{ children: ReactNode }> = ({ children }) => {
+	return (
+		<DashboardLayout>
+			<div className="grid lg:grid-cols-6 mt-12 gap-y-8">
+				<div>
+					<h3 className="font-comic-cat text-secondary text-2xl mb-5">SETTINGS</h3>
+					<nav>
+						{navItems.map(e => (
+							<Link key={e.name} href={e.path}>
+								<a className="block">{e.name}</a>
+							</Link>
+						))}
+					</nav>
+				</div>
+				<div className="col-span-5">{children}</div>
+			</div>
+		</DashboardLayout>
+	)
+}
+
+export default ProfileLayout
