a0fd9bb45fd75588c96ed99b33998b7966875953
Jared Sy <jaredblase@gmail.com>
2022-06-26 23:06:21 +0800

    add: auth types

--- types/next-auth.d.ts
+++ types/next-auth.d.ts
@@ -1,22 +1,26 @@
 import NextAuth, { DefaultSession } from 'next-auth'
 import { JWT } from 'next-auth/jwt'
 
-declare module 'next-auth' {
-	interface Session {
-		user: {
-			id: number
-			email: string
-			firstName: string
-			lastName: string
-		} & DefaultSession['user']
-	}
+interface IDetails {
+	id: number
+	email: string
+	firstName: string
+	lastName: string
 }
 
-declare module 'next-auth/jwt' {
-	interface JWT {
+declare module 'next-auth' {
+	interface Session { 
+		user: IDetails & DefaultSession['user']
+	}
+
+	interface User {
 		id: number
 		email: string
 		firstName: string
 		lastName: string
 	}
+}
+
+declare module 'next-auth/jwt' {
+	interface JWT extends IDetails {}
 }
