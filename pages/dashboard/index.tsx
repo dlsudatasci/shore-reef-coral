d3159e345287ff2941dfc36d8f9db7cb325f5dad
Jared Sy <jaredblase@gmail.com>
2022-06-30 17:45:11 +0800

    add: survey item component to display in dashboard

--- pages/dashboard/index.tsx
+++ pages/dashboard/index.tsx
@@ -1,12 +1,12 @@
-import { NextPage } from 'next';
+import { NextPage } from 'next'
 import { useSession } from 'next-auth/react'
 import Head from 'next/head'
-import Image from 'next/image';
-import { useRouter } from 'next/router';
-import { toast } from 'react-toastify';
-import Card from '../../components/card';
-import DashboardLayout from '../../components/layouts/dashboard-layout';
-import { toastErrorConfig } from '../../lib/toast-defaults';
+import Image from 'next/image'
+import { useRouter } from 'next/router'
+import { toast } from 'react-toastify'
+import DashboardLayout from '../../components/layouts/dashboard-layout'
+import SurveyList from '../../components/suvey-list'
+import { toastErrorConfig } from '../../lib/toast-defaults'
 
 const Dashboard: NextPage = () => {
 	const router = useRouter()
@@ -23,16 +23,14 @@ const Dashboard: NextPage = () => {
 			<Head>
 				<title>Reef Mo | Dashboard</title>
 			</Head>
-			<div className="flex items-center mt-20">
-				<Image src="/mask-light.png" alt="Mask Icon" width={60} height={60} />
-				<h1 className="ml-4 font-comic-cat text-secondary">Dashboard</h1>
+			<div className="grid place-items-center mt-20">
+				<div className="flex items-center border-secondary border px-8 py-4">
+					<h1 className="mr-4 font-comic-cat text-secondary">Dashboard</h1>
+					<Image src="/laptop-light.png" alt="Laptop Icon" width={60} height={60} />
+				</div>
+				<h2 className="text-secondary font-comic-cat mt-8">Team Name</h2>
 			</div>
-			<h2 className="text-secondary font-comic-cat mt-8">Team Name</h2>
-			<div className="grid md:grid-cols-3 gap-4 mt-12">
-				<Card number={5} noun="Images" verb="Submitted" />
-				<Card number={40} noun="Butterfly Fish" verb="Found" />
-				<Card number={80} noun="Target Invertebrates" verb="Found	" />
-			</div>
+			<SurveyList className="mt-8" />
 		</DashboardLayout>
 	)
 }
---
+++ pages/surveys/[id].tsx
@@ -0,0 +1,10 @@
+import { NextPage } from 'next'
+
+const Survey: NextPage = () => {
+	return (
+		<>
+		</>
+	)
+}
+
+export default Survey
