f7afd529baa6bc8983632d3e0b59c482a4865f87
Jared Sy <jaredblase@gmail.com>
2022-07-24 16:07:14 +0800

    fixed: border bug

--- components/survey-list.tsx
+++ components/survey-list.tsx
@@ -3,14 +3,16 @@ import { FC } from 'react'
 import cn from 'classnames'
 import Link from 'next/link'
 
-const style = "border-secondary group-hover:border-primary group-hover:bg-secondary border-4 flex items-center py-1.5 px-4 col-span-7 sm:col-span-8"
+const style = "border-secondary group-hover:border-primary border-r-4 group-hover:bg-secondary flex items-center py-1.5 px-4 col-span-7 sm:col-span-8"
 
+// TODO: Convert to outline
 const SurveyItem: FC = () => {
 	return (
 		<Link href="/surveys/1">
-			<div className="grid grid-cols-9 text-secondary hover:text-primary font-comic-cat text-xl cursor-pointer group hover:scale-105 transition-all">
+			<div className="outline-secondary hover:outline-primary outline grid grid-cols-9
+			 text-secondary hover:text-primary font-comic-cat text-xl cursor-pointer group hover:scale-105 transition-all">
 				<div className={cn(style, 'border-b-2')}><p>Station Name</p></div>
-				<div className="border-secondary p-2 group-hover:border-primary group-hover:bg-secondary border-4 border-l-0 row-span-2 col-span-2 sm:col-span-1 grid place-items-center">
+				<div className="p-2 group-hover:bg-secondary row-span-2 col-span-2 sm:col-span-1 grid place-items-center">
 					<div className="relative w-full h-full">
 						<Image className="!hidden group-hover:!block" src="/mask-dark.png" alt="mask icon" layout="fill" />
 						<Image className="!block group-hover:!hidden" src="/mask-light.png" alt="mask icon" layout="fill" />
