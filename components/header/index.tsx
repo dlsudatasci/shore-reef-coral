053496bb0c1c6359d8929d67119b63e2bfb00dc2
Jared Sy <jaredblase@gmail.com>
2022-07-28 16:09:42 +0800

    fix: header

--- components/header/index.tsx
+++ components/header/index.tsx
@@ -20,23 +20,22 @@ interface MobNavItemProp extends NavItemProp {
 
 export const navItems: NavItemProp[] = [
 	{ path: '/about', text: 'About' },
-	{ path: '/contribute', text: 'How to contribute', status: 'unauthenticated' },
-	{ path: '/surveys/submit', text: 'Submit a survey', status: 'authenticated' },
-	{ path: '/lessons', text: 'Lessons', status: 'authenticated' },
+	{ path: '/surveys/submit', text: 'Submit a survey' },
+	{ path: '/lessons', text: 'Lessons' },
 	{ path: '/dashboard', text: 'Dashboard', status: 'authenticated' },
 ]
 
-const MobNavItem: FC<MobNavItemProp> = ({ text, path, isHome, onClick }) => {
+const MobNavItem: FC<MobNavItemProp> = ({ text, path, onClick }) => {
 	const { pathname } = useRouter()
 
 	return (
 		<Link href={path}>
-			<a className='border-l-4 block px-3 py-2 text-base font-medium cursor-pointer' aria-current={pathname == path ? 'page' : undefined} onClick={onClick}>{text}</a>
+			<a className='border-l-2 block px-3 py-2 text-base font-medium cursor-pointer' aria-current={pathname == path ? 'page' : undefined} onClick={onClick}>{text}</a>
 		</Link>
 	)
 }
 
-const NavItem: FC<NavItemProp> = ({ text, path, isHome }) => {
+const NavItem: FC<NavItemProp> = ({ text, path }) => {
 	const { pathname } = useRouter()
 
 	return (
@@ -58,39 +57,39 @@ const Header: FC = () => {
 					<>
 						<div className="container mx-auto px-2 sm:px-6 lg:px-8 h-full">
 							<div className="relative flex items-center justify-between h-full">
-								<div className="absolute inset-y-0 left-0 flex items-center md:hidden">
+								<div className="absolute inset-y-0 left-0 flex items-center lg:hidden">
 									<Disclosure.Button className="inline-flex items-center justify-center p-2 rounded-md text-secondary hover:text-primary hover:bg-secondary focus:outline-none focus:ring-2 focus:ring-inset focus:ring-secondary" aria-controls="mobile-menu" aria-expanded="false">
 										<span className="sr-only">Open main menu</span>
 										{open ? <XIcon className="block h-6 w-6" aria-hidden="true" /> : <MenuIcon className="block h-6 w-6" aria-hidden="true" />}
 									</Disclosure.Button>
 								</div>
 
-								<div className="flex-1 md:flex-grow-0 flex items-center h-full justify-center md:items-stretch md:justify-start">
+								<div className="flex-1 lg:flex-grow-0 flex items-center h-full justify-center lg:items-stretch lg:justify-start">
 									<Link href="/" passHref>
 										<div className="flex-shrink-0 flex items-center cursor-pointer mr-6">
 											<Image className="block w-auto" src="/logo-white.png" alt="SHORE Logo" width={80} height={80} />
 										</div>
 									</Link>
 								</div>
-								<div className="hidden md:block md:ml-6">
+								<div className="hidden lg:block absolute left-1/2 -translate-x-1/2">
 									<div className="flex space-x-4 items-center h-full">
 										{navItems.flatMap(nav => nav.status == undefined || nav.status == status ?
 											<NavItem key={nav.text} text={nav.text} path={nav.path} isHome={nav.isHome} /> : []
 										)}
 									</div>
 								</div>
-								<div className="absolute inset-y-0 right-0 flex items-center pr-2 md:static md:inset-auto md:ml-6 md:pr-0">
+								<div className="absolute inset-y-0 right-0 flex items-center pr-2 lg:static lg:inset-auto lg:ml-6 lg:pr-0">
 									{status == 'authenticated' ?
 										<ProfileMenu />
 										:
-										<>
+										<div className="hidden lg:contents">
 											<Link href='/login'>
 												<a type="button" className="font-comic-cat mr-4 text-xl text-secondary">Login</a>
 											</Link>
 											<Link href='/register'>
 												<a type="button" className="btn secondary h-auto py-1.5">Sign Up</a>
 											</Link>
-										</>
+										</div>
 									}
 								</div>
 							</div>
@@ -105,9 +104,11 @@ const Header: FC = () => {
 							leaveTo="transform scale-95 opacity-0"
 						>
 
-							<Disclosure.Panel className="md:hidden relative bg-white">
+							<Disclosure.Panel className="lg:hidden relative bg-white">
 								<div className="px-2 pt-2 pb-3 space-y-1">
 									{navItems.map(nav => <MobNavItem key={nav.text} text={nav.text} path={nav.path} isHome={nav.isHome} onClick={close} />)}
+									<MobNavItem text="Login" path="/login" onClick={close} />
+									<MobNavItem text="Sign Up" path="/register" onClick={close} />
 								</div>
 							</Disclosure.Panel>
 						</Transition>
