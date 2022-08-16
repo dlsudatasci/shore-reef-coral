775eb9e8f95ae0691954f358ff13be084b941275
Jared Sy <jaredblase@gmail.com>
2022-08-16 10:54:54 +0800

    add: useRetriever

---
+++ lib/useRetriever.ts
@@ -0,0 +1,35 @@
+import useSWR, { KeyedMutator } from 'swr'
+import app from './axios-config'
+
+export function useRetriever<T>(key: string, fallback?: undefined, processor?: undefined): {
+	data: T | undefined
+	isLoading: boolean
+	isError: boolean
+	mutate: KeyedMutator<T>
+}
+
+export function useRetriever<T>(key: string, fallback: T, processor?: undefined): {
+	data: T
+	isLoading: boolean
+	isError: boolean
+	mutate: KeyedMutator<T>
+}
+
+export function useRetriever<T, V>(key: string, fallback: T, processor: (data: T) => V): {
+	data: V
+	isLoading: boolean
+	isError: boolean
+	mutate: KeyedMutator<T>
+}
+
+export function useRetriever<T, V = unknown>(key: string, fallback?: T, processor?: (data: T) => V) {
+	const { data, error, mutate } = useSWR(key, url => app.get<T>(url).then(res => res.data))
+	const temp = data === undefined ? fallback : data
+
+	return {
+		data: processor ? processor(temp as T) : temp,
+		isLoading: !data && !error,
+		isError: !!error,
+		mutate
+	}
+}
