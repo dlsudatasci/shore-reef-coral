fd793dad3ac812bbba863e00deb55831464f829a
Jared Sy <jaredblase@gmail.com>
2022-08-17 14:47:53 +0800

    fix: useRetriever key type

--- lib/useRetriever.ts
+++ lib/useRetriever.ts
@@ -1,29 +1,29 @@
-import useSWR, { KeyedMutator } from 'swr'
+import useSWR, { Key, KeyedMutator } from 'swr'
 import app from './axios-config'
 
-export function useRetriever<T>(key: string, fallback?: undefined, processor?: undefined): {
+export function useRetriever<T>(key: Key, fallback?: undefined, processor?: undefined): {
 	data: T | undefined
 	isLoading: boolean
 	isError: boolean
 	mutate: KeyedMutator<T>
 }
 
-export function useRetriever<T>(key: string, fallback: T, processor?: undefined): {
+export function useRetriever<T>(key: Key, fallback: T, processor?: undefined): {
 	data: T
 	isLoading: boolean
 	isError: boolean
 	mutate: KeyedMutator<T>
 }
 
-export function useRetriever<T, V>(key: string, fallback: T, processor: (data: T) => V): {
+export function useRetriever<T, V>(key: Key, fallback: T, processor: (data: T) => V): {
 	data: V
 	isLoading: boolean
 	isError: boolean
 	mutate: KeyedMutator<T>
 }
 
-export function useRetriever<T, V = unknown>(key: string, fallback?: T, processor?: (data: T) => V) {
-	const { data, error, mutate } = useSWR(key, url => app.get<T>(url).then(res => res.data))
+export function useRetriever<T, V = unknown>(key: Key, fallback?: T, processor?: (data: T) => V) {
+	const { data, error, mutate } = useSWR(key, (url: string) => app.get<T>(url).then(res => res.data))
 	const temp = data === undefined ? fallback : data
 
 	return {
