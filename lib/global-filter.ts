a1f605ae050fe7f600cbb62f8d022c3996e00375
Jared Sy <jaredblase@gmail.com>
2022-08-19 01:53:25 +0800

    add: global filter util

---
+++ lib/global-filter.ts
@@ -0,0 +1,34 @@
+// code from https://tanstack.com/table/v8/docs/examples/react/filters
+import { FilterFn, sortingFns, SortingFn } from '@tanstack/react-table'
+import { RankingInfo, rankItem, compareItems, Ranking } from '@tanstack/match-sorter-utils'
+
+declare module '@tanstack/table-core' {
+	interface FilterFns {
+		fuzzy: FilterFn<unknown>
+	}
+	interface FilterMeta {
+		itemRank: RankingInfo
+	}
+}
+
+export function generateFuzzyFilter(threshold: Ranking) {
+	const fuzzyFilter: FilterFn<unknown> = (row, columnId, value, addMeta) => {
+		const itemRank = rankItem(row.getValue(columnId), value, { threshold })
+		addMeta({ itemRank })
+		return itemRank.passed
+	}
+	return fuzzyFilter
+}
+
+export const fuzzySort: SortingFn<unknown> = (rowA, rowB, columnId) => {
+	let dir = 0
+
+	if (rowA.columnFiltersMeta[columnId]) {
+		dir = compareItems(
+			rowA.columnFiltersMeta[columnId]?.itemRank,
+			rowB.columnFiltersMeta[columnId]?.itemRank
+		)
+	}
+
+	return dir === 0 ? sortingFns.alphanumeric(rowA, rowB, columnId) : dir
+}
