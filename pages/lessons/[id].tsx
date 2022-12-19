dd51bacb33d5ecfb23b9c78aa6e5779bdb14ce41
TyroneStaMaria <tyrone.stamaria35@gmail.com>
2022-12-19 13:07:18 +0800

    add: scrollable lesson sidebar

--- pages/lessons/[id].tsx
+++ pages/lessons/[id].tsx
@@ -15,12 +15,14 @@ const Lesson: NextPage<InferGetStaticPropsType<typeof getStaticProps>> = ({
   lessonData,
 }) => {
   const [selected, setSelected] = useState<0 | 1>(0);
+	const lessonNumber = (lessonData.order - 1).toString().padStart(2, "0")
 
   return (
     <>
       <section className="bg-primary text-secondary px-4 py-10">
-        <div className="grid gap-6 sm:grid-cols-[1fr_220px] max-w-6xl mx-auto">
+        <div className="grid gap-6 sm:grid-cols-[1fr_220px] max-w-6xl mx-auto items-end">
           <div>
+						<h3 className="font-comic-cat">LESSON {lessonNumber}:</h3>
             <h1 className="font-comic-cat mb-4">{lessonData.title}</h1>
             <iframe
               className="w-full aspect-video"
@@ -30,13 +32,13 @@ const Lesson: NextPage<InferGetStaticPropsType<typeof getStaticProps>> = ({
               allowFullScreen
             ></iframe>
           </div>
-          <div className="overflow-y-auto relative max-h-full">
+          <div className={cn(styles["lesson-list-wrapper"])}>
             <ul>
               {lessons.map((lesson, index) => (
                 <li
                   className={cn(
                     "border-2  border-secondary px-2 py-1",
-                    lesson.id === lessonData.id ? "bg-white" : "",
+                    lesson.id === lessonData.id ? "bg-secondary" : "",
                     index === lessons.length - 1 ? "border-b-2" : "border-b-0"
                   )}
                   key={lesson.id}
@@ -49,6 +51,10 @@ const Lesson: NextPage<InferGetStaticPropsType<typeof getStaticProps>> = ({
                           : "text-secondary"
                       )}
                     >
+											<span>
+												{(lesson.order - 1).toString().padStart(2, "0")}
+											</span>
+											<br />
                       {lesson.title}
                     </a>
                   </Link>
