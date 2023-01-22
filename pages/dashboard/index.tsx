0290226086d6152b92c617eebede7ed480155333
Jared <jaredblase@gmail.com>
2023-01-23 01:04:28 +0800

    update: revise dashboard

--- pages/dashboard/index.tsx
+++ pages/dashboard/index.tsx
@@ -3,14 +3,13 @@ import { useRetriever } from '@lib/useRetriever'
 import { NextPage } from 'next'
 import { useSession } from 'next-auth/react'
 import Head from 'next/head'
-import Image from 'next/image'
 import { useRouter } from 'next/router'
 import DashboardLayout from '@components/layouts/dashboard-layout'
 import SurveyList from '@components/survey-list'
-import { UsersOnTeams } from '@prisma/client'
 import Link from 'next/link'
 import Laptop from '@components/icons/laptop'
 import Camera from '@components/icons/camera'
+import { UserTeamsAPI } from '@pages/api/users/[id]/teams'
 
 const Dashboard: NextPage = () => {
 	const router = useRouter()
@@ -20,7 +19,7 @@ const Dashboard: NextPage = () => {
 			router.replace('/login?error=Please login to continue.')
 		},
 	})
-	const { data: teams } = useRetriever<UsersOnTeams[]>(data ? `/users/${data.user.id}/teams` : null, [])
+	const { data: teams } = useRetriever<UserTeamsAPI[]>(data ? `/users/${data.user.id}/teams` : null, [])
 
 	if (status == 'loading') {
 		return <DashboardLayout><LoadingSpinner className="main-height" borderColor="border-secondary" /></DashboardLayout>
@@ -38,17 +37,12 @@ const Dashboard: NextPage = () => {
 				</div>
 			</div>
 			{teams.length !== 0 ?
-				<>
-					<h2 className="text-secondary font-comic-cat mt-8 text-center">Team Name</h2>
-					<SurveyList className="mt-8" />
-				</>
+				<SurveyList className="mt-8" teams={teams} />
 				:
 				<div className="rounded-md bg-highlight flex items-center px-6 py-3 mt-8">
 					<Camera className="fill-primary w-12 hidden sm:block" />
 					<h3 className="flex-1 text-t-highlight md:text-2xl text-base font-comic-cat">You are not yet in a volunteer team!</h3>
-					<Link href="/teams">
-						<a className="btn primary">Join a Team</a>
-					</Link>
+					<Link className="btn primary" href="/teams">Join a Team</Link>
 				</div>
 			}
 		</DashboardLayout>
