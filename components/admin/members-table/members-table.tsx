20871692551a2c0e23d8564dcc12a0839db9cd79
TyroneStaMaria <tyrone.stamaria35@gmail.com>
2023-07-24 12:30:43 +0800

    add: remove member prompt

--- components/admin/members-table/members-table.tsx
+++ components/admin/members-table/members-table.tsx
@@ -6,36 +6,17 @@ import {
   getSortedRowModel,
   useReactTable,
 } from "@tanstack/react-table";
-import { HTMLAttributes, useState } from "react";
+import { HTMLAttributes, useEffect, useState } from "react";
 import Link from "next/link";
 import { UsersSummary } from "@pages/api/users";
+import { RemoveModalProps } from "../modals/remove";
+import dynamic from "next/dynamic";
+import { createPortal } from "react-dom";
 
 const helper = createColumnHelper<UsersSummary>();
-
-const columns = [
-  helper.accessor((row) => `${row.firstName} ${row.lastName}`, { id: "name" }),
-  helper.accessor("affiliation", { header: "Affiliation" }),
-  helper.display({
-    id: "status",
-    header: "Status",
-    cell(props) {
-      return <p>Active</p>;
-    },
-  }),
-  helper.display({
-    id: "action",
-    cell: ({ row }) => (
-      <div className="flex gap-5">
-        <button className="btn bg-highlight text-t-highlight px-2 rounded-md font-sans">
-          remove
-        </button>
-        <button className="btn bg-highlight text-t-highlight px-2 rounded-md font-sans">
-          move
-        </button>
-      </div>
-    ),
-  }),
-];
+const RemoveModal = dynamic<RemoveModalProps>(() =>
+  import("@components/admin/modals/remove").then((mod) => mod.RemoveModal)
+);
 
 type MembersTableProps = {
   data: UsersSummary[];
@@ -43,6 +24,38 @@ type MembersTableProps = {
 
 export function MembersTable({ data, ...props }: MembersTableProps) {
   const [sorting, setSorting] = useState<SortingState>([]);
+  const [id, setId] = useState<number | string | undefined>(undefined);
+
+  const columns = [
+    helper.accessor((row) => `${row.firstName} ${row.lastName}`, {
+      id: "name",
+    }),
+    helper.accessor("affiliation", { header: "Affiliation" }),
+    helper.display({
+      id: "status",
+      header: "Status",
+      cell(props) {
+        return <p>Active</p>;
+      },
+    }),
+    helper.display({
+      id: "action",
+      cell: ({ row }) => (
+        <div className="flex gap-5">
+          <button
+            className="btn bg-highlight text-t-highlight px-2 rounded-md font-sans"
+            onClick={() => setId(Number(row.id))}
+          >
+            remove
+          </button>
+          <button className="btn bg-highlight text-t-highlight px-2 rounded-md font-sans">
+            move
+          </button>
+        </div>
+      ),
+    }),
+  ];
+
   const table = useReactTable<UsersSummary>({
     data,
     columns,
@@ -54,48 +67,74 @@ export function MembersTable({ data, ...props }: MembersTableProps) {
     getSortedRowModel: getSortedRowModel(),
   });
 
+  async function onRemoveClick() {
+    console.log("Remove");
+  }
+
   return (
-    <table {...props}>
-      <thead>
-        <tr className="bg-secondary text-primary font-comic-cat text-xl text-left">
-          {table.getFlatHeaders().map((header) => (
-            <th key={header.id} className="font-normal py-3 px-4">
-              {header.isPlaceholder ? null : (
-                <div
-                  className={
-                    header.column.getCanSort()
-                      ? "cursor-pointer select-none"
-                      : ""
-                  }
-                  onClick={header.column.getToggleSortingHandler()}
-                >
-                  {flexRender(
-                    header.column.columnDef.header,
-                    header.getContext()
-                  )}
-                  <div className="w-4 inline-block">
-                    {{
-                      asc: " 🔼",
-                      desc: " 🔽",
-                    }[header.column.getIsSorted() as string] ?? ""}
+    <div>
+      {createPortal(
+        <RemoveModal
+          title={`Remove ${
+            table
+              .getRowModel()
+              .rows.find((row) => row.id)
+              ?.getVisibleCells()[0].row._valuesCache.name
+          } ?`}
+          message={`Are you sure you want to remove ${
+            table
+              .getRowModel()
+              .rows.find((row) => row.id)
+              ?.getVisibleCells()[0].row._valuesCache.name
+          } from the team?`}
+          isOpen={id !== undefined}
+          close={() => setId(undefined)}
+          onAction={onRemoveClick}
+        />,
+        document.body
+      )}
+      <table {...props}>
+        <thead>
+          <tr className="bg-secondary text-primary font-comic-cat text-xl text-left">
+            {table.getFlatHeaders().map((header) => (
+              <th key={header.id} className="font-normal py-3 px-4">
+                {header.isPlaceholder ? null : (
+                  <div
+                    className={
+                      header.column.getCanSort()
+                        ? "cursor-pointer select-none"
+                        : ""
+                    }
+                    onClick={header.column.getToggleSortingHandler()}
+                  >
+                    {flexRender(
+                      header.column.columnDef.header,
+                      header.getContext()
+                    )}
+                    <div className="w-4 inline-block">
+                      {{
+                        asc: " 🔼",
+                        desc: " 🔽",
+                      }[header.column.getIsSorted() as string] ?? ""}
+                    </div>
                   </div>
-                </div>
-              )}
-            </th>
-          ))}
-        </tr>
-      </thead>
-      <tbody className="text-t-highlight [&_td]:py-3 [&_td]:px-4 [&>tr:nth-child(even)]:bg-accent-1">
-        {table.getRowModel().rows.map((row) => (
-          <tr key={row.id}>
-            {row.getVisibleCells().map((cell) => (
-              <td key={cell.id}>
-                {flexRender(cell.column.columnDef.cell, cell.getContext())}
-              </td>
+                )}
+              </th>
             ))}
           </tr>
-        ))}
-      </tbody>
-    </table>
+        </thead>
+        <tbody className="text-t-highlight [&_td]:py-3 [&_td]:px-4 [&>tr:nth-child(even)]:bg-accent-1">
+          {table.getRowModel().rows.map((row) => (
+            <tr key={row.id}>
+              {row.getVisibleCells().map((cell) => (
+                <td key={cell.id}>
+                  {flexRender(cell.column.columnDef.cell, cell.getContext())}
+                </td>
+              ))}
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </div>
   );
 }
