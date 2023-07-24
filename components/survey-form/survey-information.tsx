164e9a8de8115e54a692807d027ee880a9c8b1df
Jared Sy <jaredblase@gmail.com>
2023-07-25 00:46:32 +0800

    update: survey information

--- components/survey-form/survey-information.tsx
+++ components/survey-form/survey-information.tsx
@@ -1,25 +1,45 @@
 import { FC, useEffect } from 'react'
 import { yupResolver } from '@hookform/resolvers/yup'
 import { useForm } from 'react-hook-form'
-import { surveyInfoSchema, ISurveyInformation } from '../../models/survey'
+import { surveyInfoSchema, ISurveyInformation, MANAGEMENT_TYPES } from '../../models/survey'
 import useSurveyStore, { Survey } from '../../stores/survey-store'
-import shallow from 'zustand/shallow'
+import { shallow } from 'zustand/shallow'
+import useSWRImmutable from 'swr/immutable'
+import axios from 'axios'
+import LoadingSpinner from '@components/loading-spinner'
 
 const storeSelector = (state: Survey) => [state.surveyInfo, state.setSurveyInfo] as const
 
 const SurveyInformation: FC<{ submitHandler: () => void }> = ({ submitHandler }) => {
 	const [surveyInfo, setSurveyInfo] = useSurveyStore(storeSelector, shallow)
+	const { data: locations, isLoading } = useSWRImmutable('/bgy-masterlist.json', url => axios.get(url).then(res => res.data))
 
-	const { register, handleSubmit, formState: { errors }} = useForm<ISurveyInformation>({
+	const { register, handleSubmit, formState: { errors }, watch, setValue } = useForm<ISurveyInformation>({
 		resolver: yupResolver(surveyInfoSchema),
 		// @ts-ignore 
 		defaultValues: { ...surveyInfo, datetime: surveyInfo.datetime.toLocaleString('sv').slice(0, -3).replace(' ', 'T') },
 	})
 
+	const province = watch('province')
+	const town = watch('town')
+
+	useEffect(() => {
+		setValue('town', '')
+		setValue('barangay', '')
+	}, [province])
+
+	useEffect(() => {
+		setValue('barangay', '')
+	}, [town])
+
 	const onSubmit = handleSubmit(data => {
 		setSurveyInfo(data)
 		submitHandler()
 	})
+
+	if (isLoading) {
+		return <LoadingSpinner borderColor="border-highlight" />
+	}
 
 	return (
 		<form id="survey-form" onSubmit={onSubmit}>
@@ -35,14 +55,12 @@ const SurveyInformation: FC<{ submitHandler: () => void }> = ({ submitHandler })
 			</div>
 			<div className="control">
 				<label htmlFor="start-coordinates" className="text-secondary">starting corner coordinates</label>
-				<input type="number" id="start-coordinates" {...register('startCorner')} />
+				<input type="text" id="start-coordinates" placeholder="12.3456, 7.8901" {...register('startCorner')} />
 				<p className="error text-secondary">{errors.startCorner?.message}</p>
 			</div>
 			<div className="control">
 				<label htmlFor="end-coordinates" className="text-secondary">ending corner coordinates</label>
-				<input type="number" id="end-coordinates" {...register('endCorner', {
-					setValueAs: v => v === '' ? undefined : parseInt(v)
-				})} />
+				<input type="text" id="end-coordinates" placeholder="12.3456, 7.8901" {...register('endCorner')} />
 				<p className="error text-secondary">{errors.endCorner?.message}</p>
 			</div>
 			<div className="control">
@@ -52,29 +70,42 @@ const SurveyInformation: FC<{ submitHandler: () => void }> = ({ submitHandler })
 			</div>
 			<div className="control">
 				<label htmlFor="province" className="text-secondary">province</label>
-				<select id="province" {...register('province')}>
-					<option value="Manila">Manila</option>
+				<select id="province" {...register('province')} defaultValue="">
+					<option value="" disabled defaultChecked>-SELECT PROVINCE-</option>
+					{locations[0].map((l: string) => (
+						<option key={l} value={l}>{l}</option>
+					))}
 				</select>
 				<p className="error text-secondary">{errors.province?.message}</p>
 			</div>
 			<div className="control">
 				<label htmlFor="town" className="text-secondary">town</label>
-				<select id="town" {...register('town')}>
-					<option value="Manila">Manila</option>
+				<select id="town" {...register('town')} disabled={!locations[1][province]} defaultValue="">
+					<option value="" disabled defaultChecked>-SELECT TOWN-</option>
+					{
+						locations[1][province]?.map((t: string) => (
+							<option key={t} value={t}>{t}</option>
+						))
+					}
 				</select>
 				<p className="error text-secondary">{errors.town?.message}</p>
 			</div>
 			<div className="control">
 				<label htmlFor="barangay" className="text-secondary">barangay</label>
-				<select id="barangay" {...register('barangay')}>
-					<option value="Manila">Manila</option>
+				<select id="barangay" {...register('barangay')} disabled={!locations[2][province + town]} defaultValue="">
+					<option value="" disabled defaultChecked>-SELECT BARANGAY-</option>
+					{
+						locations[2][province + town]?.map((b: string) => (
+							<option key={b} value={b}>{b}</option>
+						))
+					}
 				</select>
 				<p className="error text-secondary">{errors.barangay?.message}</p>
 			</div>
 			<div className="control">
 				<label htmlFor="management" className="text-secondary">type of management</label>
 				<select id="management" {...register('management')}>
-					<option value="Manila">Manila</option>
+					{MANAGEMENT_TYPES.map(d => <option key={d} value={d}>{d}</option>)}
 				</select>
 				<p className="error text-secondary">{errors.management?.message}</p>
 			</div>
--- models/survey.ts
+++ models/survey.ts
@@ -1,27 +1,20 @@
-import { object, number, string, date } from 'yup'
+import { object, string, date, InferType, mixed } from 'yup'
 
-export interface ISurveyInformation {
-	datetime: Date
-	station: string
-	startCorner: number
-	endCorner?: number
-	gps: string
-	province: string
-	town: string
-	barangay: string
-	management: string
-	others?: string
+export const MANAGEMENT_TYPES = ['None', 'Locally managed MPA', 'Nationally managed MPA'] as const
+
+export interface ISurveyInformation extends InferType<typeof surveyInfoSchema> {
+	management: typeof MANAGEMENT_TYPES[number]
 }
 
 export const surveyInfoSchema = object({
 	datetime: date().typeError('Invalid date').required(),
 	station: string().required(' Station name is required.'),
-	startCorner: number().typeError('Start corner is required.').required(),
-	endCorner: number().optional(),
+	startCorner: string().required('Start coordinate is required!').matches(/\d+.\d{3,4}, \d+.\d{3,4}/, { message: 'Invalid format! Should be: x.xxxx, x.xxxx' }),
+	endCorner: string().required('End coordinate is required!').matches(/\d+.\d{3,4}, \d+.\d{3,4}/, { message: 'Invalid format! Should be: x.xxxx, x.xxxx' }),
 	gps: string().required('GPS datum is required.'),
 	province: string().required('Province is required.'),
 	town: string().required('Town is required.'),
 	barangay: string().required('Barangay is required.'),
-	management: string().required('Management is required.'),
+	management: mixed<typeof MANAGEMENT_TYPES[number]>().oneOf([...MANAGEMENT_TYPES], 'Invalid option!').required('Management is required.'),
 	others: string().optional(),
 }).required()
