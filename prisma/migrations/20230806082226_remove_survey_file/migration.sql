e969dda3c2ccda599f45d4105146c8abd79fa519
Jared Sy <jaredblase@gmail.com>
2023-08-06 19:09:43 +0800

    update: schema and migration

---
+++ prisma/migrations/20230806082226_remove_survey_file/migration.sql
@@ -0,0 +1,23 @@
+/*
+  Warnings:
+
+  - You are about to drop the column `verified` on the `survey` table. All the data in the column will be lost.
+  - You are about to drop the `surveyfile` table. If the table is not empty, all the data it contains will be lost.
+
+*/
+-- DropForeignKey
+ALTER TABLE `surveyfile` DROP FOREIGN KEY `SurveyFile_surveyId_fkey`;
+
+-- AlterTable
+ALTER TABLE `coralassessment` ADD COLUMN `volunteer5` VARCHAR(191) NULL,
+    ADD COLUMN `volunteer6` VARCHAR(191) NULL;
+
+-- AlterTable
+ALTER TABLE `survey` DROP COLUMN `verified`,
+    ADD COLUMN `volunteer5` VARCHAR(191) NULL,
+    ADD COLUMN `volunteer6` VARCHAR(191) NULL,
+    MODIFY `additionalInfo` VARCHAR(191) NOT NULL DEFAULT '',
+    MODIFY `status` VARCHAR(191) NOT NULL DEFAULT 'PENDING';
+
+-- DropTable
+DROP TABLE `surveyfile`;
