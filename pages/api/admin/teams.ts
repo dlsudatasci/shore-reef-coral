aa36f5e0a9781712c8304203da9fa092296c0721
Dominic Sia <dominic_ethan_sia@dlsu.edu.ph>
2024-06-18 22:53:09 +0800

    add: filters for accepted/rejected teams

--- pages/api/admin/teams.ts
+++ pages/api/admin/teams.ts
@@ -1,5 +1,6 @@
 import { NextApiRequest, NextApiResponse } from 'next'
-import { Prisma } from "@prisma/client";
+import { Prisma, TeamStatus } from "@prisma/client";
+import { FieldRef } from '@prisma/client/runtime/library';
 
 export type TeamsSummary = Prisma.TeamGetPayload<typeof teamsSummary>
 
@@ -8,9 +9,10 @@ const teamsSummary = Prisma.validator<Prisma.TeamDefaultArgs>()({
     id: true,
     name: true,
     province: true,
-    isVerified: true,
     town: true,
     affiliation: true,
+    isVerified: true,
+    status: true,
     UsersOnTeam: {
       select: {
         userId: true,
@@ -27,28 +29,43 @@ const teamsSummary = Prisma.validator<Prisma.TeamDefaultArgs>()({
 })
 
 const handler = async (req: NextApiRequest, res: NextApiResponse) => {
-  const { method } = req
+  const { method, query } = req;
 
   try {
     switch (method) {
       case 'GET': {
+        const { name, town, province, status } = query;
+        const teamStatus = status as TeamStatus;
+
+        // Initialize the where clause
+        const where: Prisma.TeamWhereInput = {};
+
+        // Apply filters
+        if (name) where.name = { contains: String(name) };
+        if (province) where.province = { equals: String(province) };
+        if (town) where.town = { contains: String(town) };
+        if (status) where.status = { equals: status as TeamStatus };
+
+        // Execute the query with the built where clause
         const teams = await prisma.team.findMany({
           ...teamsSummary,
-        })
-        res.json(teams)
-        break
+          where,
+        });
+
+        res.json(teams);
+        break;
       }
 
       default:
-        res.setHeader('Allow', ['GET'])
-        res.status(405).end(`Method ${method} Not Allowed`)
+        res.setHeader('Allow', ['GET']);
+        res.status(405).end(`Method ${method} Not Allowed`);
     }
   } catch (err) {
-    console.error(err)
-    res.status(500)
+    console.error(err);
+    res.status(500).json({ error: 'Internal Server Error' });
   } finally {
-    res.end()
+    res.end();
   }
-}
+};
 
 export default handler
