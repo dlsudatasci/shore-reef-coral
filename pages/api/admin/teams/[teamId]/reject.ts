560e69dff3189af18beb781558f8b33e4a00bcc4
Dominic Sia <dominic_ethan_sia@dlsu.edu.ph>
2024-06-23 00:42:30 +0800

    add: reject teams function

---
+++ pages/api/admin/teams/[teamId]/reject.ts
@@ -0,0 +1,53 @@
+import { NextApiRequest, NextApiResponse } from 'next'
+import { getServerSession } from 'next-auth/next'
+import prisma from '@lib/prisma'
+import { authOptions } from '@pages/api/auth/[...nextauth]'
+
+const handler = async (req: NextApiRequest, res: NextApiResponse) => {
+    const { method } = req
+	const teamId = Number(req.query.teamId)
+    const reason = req.body.reason
+
+    try {
+        switch (method) {
+            case 'POST': {
+                if (!reason || reason.trim() === '') {
+                    return res.status(400).json({ error: 'Rejection reason is required.' });
+                }
+
+                const session = await getServerSession(req, res, authOptions)
+                if (!session) return res.status(401)
+
+                // Authenticate admin
+                const count = await prisma.user.count({
+                    where: {
+                        id: session.user.id,
+                        isAdmin: true
+                    }
+                })
+                if (!count) return res.status(401)
+
+                await prisma.$transaction([
+                    prisma.team.update({
+                        where: { id: teamId },
+                        data: { status: 'REJECTED' }
+                    }),
+                    prisma.rejectionReason.create({
+                        data: {
+                            teamId: teamId,
+                            reason: reason
+                        }
+                    })
+                ])
+
+                res.status(200).json({ message: 'Team has been rejected!' })
+                break
+            }
+        }
+    } catch (error) {
+        console.error(error)
+        res.status(500)
+    }
+}
+
+export default handler
