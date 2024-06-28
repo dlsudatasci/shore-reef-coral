15d66292dd66ddd2b0b5328a76ad0f59079f667e
Dominic Sia <dominic_ethan_sia@dlsu.edu.ph>
2024-06-28 12:22:30 +0800

    add: confirmation modal for verifying teams

--- components/admin/team-tabs/team-info-tab.tsx
+++ components/admin/team-tabs/team-info-tab.tsx
@@ -9,6 +9,14 @@ import { toastAxiosError } from "@lib/utils";
 import { toast } from "react-toastify";
 import { toastSuccessConfig } from "@lib/toast-defaults";
 
+//Confirmation Modal
+import { createPortal } from "react-dom";
+import { ConfirmationModalProps } from "@components/confirmation-modal";
+import dynamic from "next/dynamic";
+const ConfirmationModal = dynamic<ConfirmationModalProps>(() =>
+  import("@components/confirmation-modal").then((mod) => mod.ConfirmationModal)
+);
+
 type TeamInfoTabProps = {
   data: any;
 };
@@ -35,11 +43,27 @@ const TeamInfoTab: FC<TeamInfoTabProps> = ({ data }) => {
     } catch (error) {
       toastAxiosError(error);
       setTeamInfo(teamInfo);
+    } finally {
+      setIsVerifyModalOpen(false);
     }
   };
 
+  const [isVerifyModalOpen, setIsVerifyModalOpen] = useState(false);
+
   return (
     <>
+      {createPortal(
+        <ConfirmationModal
+          title="Verify Team"
+          message={`Are you sure you want to verify the team ${data.name}?`}
+          isOpen={isVerifyModalOpen}
+          close={() => {
+            setIsVerifyModalOpen(false);
+          }}
+          onAction={onVerify}
+        />,
+        document.body
+      )}
       <div className="grid grid-cols-3 gap-y-10 mt-8">
         {teamInfo.map((item, index) => (
           <div
@@ -52,7 +76,7 @@ const TeamInfoTab: FC<TeamInfoTabProps> = ({ data }) => {
         ))}
       </div>
       {teamInfo.find(item => item.name === "Verified")?.value === "No" ? (
-        <button className="bg-primary text-secondary rounded-full px-8 py-2 my-12" onClick={onVerify}>
+        <button className="bg-primary text-secondary rounded-full px-8 py-2 my-12" onClick={() => {setIsVerifyModalOpen(true)}}>
           VERIFY TEAM
         </button>
       ) : null}
