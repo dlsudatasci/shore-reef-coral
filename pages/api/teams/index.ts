d0197bb49538eb0dfc54bc26f650713260cdc10c
Dane Chan <dane_chan@dlsu.edu.ph>
2024-07-02 17:08:11 +0800

    feat: added pending filter

--- pages/api/teams/index.ts
+++ pages/api/teams/index.ts
@@ -22,7 +22,7 @@ export type LeaderNamePayload = Prisma.UsersOnTeamsGetPayload<typeof selectLeade
 const selectTeamProfile = Prisma.validator<Prisma.TeamDefaultArgs>()({
 	select: {
 		id: true,
-		town: true, 
+		town: true,
 		name: true,
 		province: true,
 		isVerified: true,
@@ -55,7 +55,10 @@ const handler = async (req: NextApiRequest, res: NextApiResponse) => {
 						...selectTeamProfile,
 						where: {
 							UsersOnTeam: {
-								none: { userId: session.user.id }
+								none: { 
+									userId: session.user.id,
+									status: 'ACCEPTED'
+								 }
 							}
 						}
 					})
@@ -84,7 +87,25 @@ const handler = async (req: NextApiRequest, res: NextApiResponse) => {
 						...selectTeamProfile,
 						where: {
 							UsersOnTeam: {
-								some: { userId: session.user.id }
+								some: { 
+									userId: session.user.id,
+									status: 'ACCEPTED'
+								 }
+							}
+						}
+					})
+					return res.json(teams)
+				}
+
+				if (query.filter === 'pending') {
+					const teams = await prisma.team.findMany({
+						...selectTeamProfile,
+						where: {
+							UsersOnTeam: {
+								some: { 
+									userId: session.user.id,
+									status: 'PENDING'
+								 }
 							}
 						}
 					})
