270004e87e7a5f3b4cffe1de587ca018c1d996df
Dominic Sia <dominic_ethan_sia@dlsu.edu.ph>
2024-07-19 21:48:18 +0800

    fix: resolved errors in the page

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -10,7 +10,6 @@ class MyDocument extends Document {
 					<link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
 					<link rel="manifest" href="/site.webmanifest" />
 					<link href="https://fonts.googleapis.com/css2?family=Josefin+Sans:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;1,100;1,200;1,300;1,400;1,500;1,600;1,700&display=swap" rel="stylesheet" />
-					<link rel="preload" href="/bgy-masterlist.json" as="fetch" crossOrigin="anonymous" />
 				</Head>
 				<body>
 					<Main />
