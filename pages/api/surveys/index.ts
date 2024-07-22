a41f201108e5b81a9996884d1eda88d7c1ac939e
Dane Chan <dane_chan@dlsu.edu.ph>
2024-07-22 19:56:08 +0800

    new: now unzips zip files

--- pages/api/surveys/index.ts
+++ pages/api/surveys/index.ts
@@ -1,193 +1,240 @@
-import { NextApiRequest, NextApiResponse } from 'next'
-import { getServerSession } from 'next-auth/next'
-import prisma from '@lib/prisma'
-import { authOptions } from '../auth/[...nextauth]'
-import formidable, { File } from 'formidable'
-import { parseFormidableOutput } from '@lib/utils'
-import minio from '@lib/minio'
-import { surveyInfoSchema } from '@models/survey'
-import { teamInfoSchema } from '@models/team'
-import { UploadedObjectInfo } from 'minio'
-import { SurveyDataType } from '@prisma/client'
+import { NextApiRequest, NextApiResponse } from 'next';
+import { getServerSession } from 'next-auth/next';
+import prisma from '@lib/prisma';
+import { authOptions } from '../auth/[...nextauth]';
+import formidable, { File } from 'formidable';
+import { parseFormidableOutput } from '@lib/utils';
+import minio from '@lib/minio';
+import { surveyInfoSchema } from '@models/survey';
+import { teamInfoSchema } from '@models/team';
+import { UploadedObjectInfo } from 'minio';
+import { SurveyDataType } from '@prisma/client';
+
+import unzipper from 'unzipper';
+import fs from 'fs';
 
 export const config = {
-	api: {
-		bodyParser: false
-	}
-}
+  api: {
+    bodyParser: false
+  }
+};
 
 function getUploader(bucket: string, surveyId: number) {
-	return (uploadPath: string, file: File) => {
-		const ext = file.newFilename.split('.')[1]
-		return minio.fPutObject(bucket, `${surveyId}/${uploadPath}.${ext}`, file.filepath)
-	}
+  return (uploadPath: string, file: File) => {
+    const ext = file.newFilename.split('.').pop();
+    return minio.fPutObject(bucket, `${surveyId}/${uploadPath}.${ext}`, file.filepath);
+  };
 }
 
 const handler = async (req: NextApiRequest, res: NextApiResponse) => {
-	const { method } = req
+  const { method } = req;
 
-	try {
-		switch (method) {
-			case 'POST': {
-				// Session authentication
-				const session = await getServerSession(req, res, authOptions)
-				if (!session) return res.status(401)
+  try {
+    switch (method) {
+      case 'POST': {
+        // Session authentication
+        const session = await getServerSession(req, res, authOptions);
+        if (!session) return res.status(401).end();
 
-				// Formidable file parsing
-				const form = formidable({ keepExtensions: true, multiples: true, maxFileSize: 300 * 1024 * 1024 })
-				const [fields, files] = await form.parse(req)
-				const parsedData = parseFormidableOutput(fields)
-				const { uploads: fileData } = parseFormidableOutput(files)
+        // Formidable file parsing
+        const form = formidable({ keepExtensions: true, multiples: true, maxFileSize: 300 * 1024 * 1024 });
+        const [fields, files] = await form.parse(req);
+        const parsedData = parseFormidableOutput(fields);
+        const { uploads: fileData } = parseFormidableOutput(files);
 
-				// Get, validate, and format survey and team information
-				const [surveyInfo, team] = await Promise.all([
-					surveyInfoSchema.validate(parsedData.surveyInfo),
-					teamInfoSchema.validate(parsedData.team),
-				])
+        if (fileData.zip && fileData.zip[0]) {
+          const zipPath = fileData.zip[0].filepath;
 
-				const { startCorner, endCorner, ...data } = surveyInfo
-				const [startLongitude, startLatitude] = startCorner.split(', ').map(n => parseFloat(n))
-				const [secondLongitude, secondLatitude] = endCorner.split(', ').map(n => parseFloat(n))
+          console.log('Zip file path:', zipPath);
 
-				// Prisma transaction
-				await prisma.$transaction(async (prisma) => {
-					const { id: surveyId } = await prisma.survey.create({
-						select: {
-							id: true,
-						},
-						data: {
-							startLongitude,
-							startLatitude,
-							secondLongitude,
-							secondLatitude,
-							...data,
-							...team,
-							submissionType: parsedData.uploads.submissionType,
-							tag: parsedData.uploads.submissionType === 'MANUAL' ? 'Photos only' : 'With data forms',
-							dataType: data.dataType as SurveyDataType,
-							dbSurveyNum: 'ALWAN',
-							uploaderId: session.user.id,
-						},
-					});
-		  
-					const surveyNumber = surveyId.toString().padStart(4, '0');
-					const formattedSurveyNumber = `ALWAN-${surveyNumber}`;
-		  
-					await prisma.survey.update({
-						where: {
-							id: surveyId,
-						},
-						data: {
-							dbSurveyNum: formattedSurveyNumber,
-						},
-					});
-		  
-					let imgCount: number;
-					if (fileData.imageUpload) {
-						imgCount = Object.keys(fileData.imageUpload).length;
-					} else {
-						imgCount = 1;
-					}
-					
-					const { id: c30ImageSetId } = await prisma.c30ImageSet.create({
-						data: {
-							surveyId: surveyId,
-							imageCount: imgCount,
-						},
-					});
-					
-					await prisma.coralAssessment.create({
-						data: {
-							imageSetId: c30ImageSetId,
-							...team,
-						}
-					});
+          if (!fs.existsSync(zipPath)) {
+            console.error('Zip file does not exist');
+            return res.status(400).json({ error: 'Zip file does not exist' });
+          }
 
-					switch (parsedData.uploads.submissionType) {
-						case 'CPCE': 
-							await prisma.surveyFile.create({
-								data: {
-									surveyId: surveyId,
-									CPCEFilePath: fileData.zip[0].originalFilename,
-								}
-							});
-							break;
-						
-						case 'ALWAN': 
-							await prisma.surveyFile.create({
-								data: {
-									surveyId: surveyId,
-									excelFilePath: fileData.zip[0].originalFilename,
-								}
-							});
-							break;
-						
-						case 'MANUAL':
-							const surveyGuideCount = Object.keys(fileData.surveyGuides).length;
-							var i;
-							for (i = 0; i < surveyGuideCount; i++) {
-								await prisma.surveyGuideImage.create({
-									data: {
-										surveyId: surveyId,
-										fileName: fileData.surveyGuides[i].originalFilename,
-									}
-								});
-							}
-							
-							await prisma.coralDatasheetImage.create({
-								data: {
-									surveyId: surveyId,
-									fileName: fileData.coralDataSheet[0].originalFilename,
-								}
-							});
-							break;
-					}
-				});
+          try {
+            fs.accessSync(zipPath, fs.constants.R_OK);
+          } catch (err) {
+            console.error('Zip file is not readable:', err);
+            return res.status(400).json({ error: 'Zip file is not readable' });
+          }
 
-				// const { uploads: fileData } = parseFormidableOutput(files)
-				// const upload = getUploader(process.env.MINIO_PRIVATE_BUCKET, surveyId)
-				// const uploadPromises: Promise<UploadedObjectInfo>[] = []
+          const extractedFiles: { [key: string]: File } = {};
 
-				// switch (parsedData.uploads.submissionType) {
-				// 	case 'CPCE': {
-				// 		uploadPromises.push(upload('data/cpce', fileData.zip[0]))
-				// 		break
-				// 	}
+          await new Promise<void>((resolve, reject) => {
+            fs.createReadStream(zipPath)
+              .pipe(unzipper.Parse())
+              .on('entry', (entry) => {
+                const fileName = entry.path;
+                const fileType = entry.type;
+                console.log(fileName)
+                if (fileName.includes('/')) {
+                  console.log(fileName)
+                  console.error(`Invalid file name: "${fileName}" contains a backslash.`);
+                  entry.autodrain();
+                  return;
+                }
+          
+                if (fileType === 'File') {
+                  const tempPath = `extracted/${fileName}`;
+                  const writeStream = fs.createWriteStream(tempPath);
+          
+                  entry.pipe(writeStream);
+          
+                  writeStream.on('finish', () => {
+                    extractedFiles[fileName] = {
+                      filepath: tempPath,
+                      newFilename: fileName,
+                      originalFilename: fileName,
+                      mimetype: 'application/octet-stream',
+                      size: fs.statSync(tempPath).size
+                    } as File;
+                    console.log(`Extracted and saved file: ${fileName}`);
+                  });
+          
+                  writeStream.on('error', (err) => {
+                    console.error('Error writing extracted file:', err);
+                    reject(err);
+                  });
+                } else {
+                  entry.autodrain();
+                }
+              })
+              .on('finish', () => {
+                console.log('Extracted files:', Object.keys(extractedFiles));
+                fileData.imageUpload = extractedFiles;
+                resolve();
+              })
+              .on('error', (err) => {
+                console.error('Error during unzip:', err);
+                reject(err);
+              });
+          });
+        }
 
-				// 	case 'ALWAN': {
-				// 		uploadPromises.push(upload('img/zip', fileData.zip[0]))
-				// 		uploadPromises.push(upload('data/alwan-data-form', fileData.alwanDataForm[0]))
-				// 		break
-				// 	}
+        const [surveyInfo, team] = await Promise.all([
+          surveyInfoSchema.validate(parsedData.surveyInfo),
+          teamInfoSchema.validate(parsedData.team),
+        ]);
 
-				// 	case 'MANUAL':
-				// 		uploadPromises.push(upload('img/zip', fileData.zip[0]))
-				// 		uploadPromises.push(upload('img/coral-data-sheet', fileData.coralDataSheet[0]))
+        const { startCorner, endCorner, ...data } = surveyInfo;
+        const [startLongitude, startLatitude] = startCorner.split(', ').map(n => parseFloat(n));
+        const [secondLongitude, secondLatitude] = endCorner.split(', ').map(n => parseFloat(n));
 
-				// 		for (const [key, value] of Object.entries<File>(fileData.surveyGuides)) {
-				// 			uploadPromises.push(upload(`img/survey-guides-${key}`, value))
-				// 		}
-				// 		break
-				// }
+        // Prisma transaction
+        await prisma.$transaction(async (prisma) => {
+          const { id: surveyId } = await prisma.survey.create({
+            select: {
+              id: true,
+            },
+            data: {
+              startLongitude,
+              startLatitude,
+              secondLongitude,
+              secondLatitude,
+              ...data,
+              ...team,
+              submissionType: parsedData.uploads.submissionType,
+              tag: parsedData.uploads.submissionType === 'MANUAL' ? 'Photos only' : 'With data forms',
+              dataType: data.dataType as SurveyDataType,
+              dbSurveyNum: 'ALWAN',
+              uploaderId: session.user.id,
+            },
+          });
 
-				// await Promise.all(uploadPromises)
+          const surveyNumber = surveyId.toString().padStart(4, '0');
+          const formattedSurveyNumber = `ALWAN-${surveyNumber}`;
 
-				// const data = await (await fetch('http://ccscloud2.dlsu.edu.ph:22302/app/extract')).json()
-				// console.log(data)
+          await prisma.survey.update({
+            where: {
+              id: surveyId,
+            },
+            data: {
+              dbSurveyNum: formattedSurveyNumber,
+            },
+          });
 
-				break
-			}
+          if (fileData.imageUpload) {
+            const imgCount = Object.keys(fileData.imageUpload).length;
 
-			default:
-				res.setHeader('Allow', ['POST'])
-				res.status(405).end(`Method ${method} Not Allowed`)
-		}
-	} catch (err) {
-		console.error(err)
-		res.status(500)
-	} finally {
-		res.end()
-	}
-}
+            const { id: c30ImageSetId } = await prisma.c30ImageSet.create({
+              data: {
+                surveyId: surveyId,
+                imageCount: imgCount,
+              },
+            });
 
-export default handler
+            await prisma.coralAssessment.create({
+              data: {
+                imageSetId: c30ImageSetId,
+                ...team,
+              }
+            });
+
+            for (const [fileName, file] of Object.entries(fileData.imageUpload)) {
+              await prisma.c30Image.create({
+                data: {
+                  imageSetId: c30ImageSetId,
+                  fileName: fileName,
+                }
+              });
+            }
+          }
+
+          switch (parsedData.uploads.submissionType) {
+            case 'CPCE': 
+              await prisma.surveyFile.create({
+                data: {
+                  surveyId: surveyId,
+                  CPCEFilePath: fileData.zip[0].originalFilename,
+                }
+              });
+              break;
+            
+            case 'ALWAN': 
+              await prisma.surveyFile.create({
+                data: {
+                  surveyId: surveyId,
+                  excelFilePath: fileData.zip[0].originalFilename,
+                }
+              });
+              break;
+            
+            case 'MANUAL':
+              const surveyGuideCount = Object.keys(fileData.surveyGuides).length;
+              for (let i = 0; i < surveyGuideCount; i++) {
+                await prisma.surveyGuideImage.create({
+                  data: {
+                    surveyId: surveyId,
+                    fileName: fileData.surveyGuides[i].originalFilename,
+                  }
+                });
+              }
+              
+              await prisma.coralDatasheetImage.create({
+                data: {
+                  surveyId: surveyId,
+                  fileName: fileData.coralDataSheet[0].originalFilename,
+                }
+              });
+              break;
+          }
+        });
+
+        res.status(200).json({ success: 'Files processed successfully' });
+        break;
+      }
+
+      default:
+        res.setHeader('Allow', ['POST']);
+        res.status(405).end(`Method ${method} Not Allowed`);
+    }
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({ error: 'Internal Server Error' });
+  } finally {
+    res.end();
+  }
+};
+
+export default handler;
